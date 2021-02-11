433ff889ecf1e99ac666e3aa18dc92026a16655b
William James <52788612+william-james-pj@users.noreply.github.com>
2021-02-11 16:44:37 -0300

    init project

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <Layout />
+</template>
+
+<script>
+import Layout from "./views/Layout";
+
+export default {
+    name: "App",
+    components: {
+        Layout
+    }
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@300;500;700&display=swap");
+* {
+    margin: 0;
+    padding: 0;
+    outline: none;
+    box-sizing: border-box;
+    font-family: "Roboto", sans-serif;
+}
+body {
+    width: 100%;
+    height: 100%;
+}
+</style>
